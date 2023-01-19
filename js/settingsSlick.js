6c6a951602211725dc29bbb4d461006661cef8fc
K0ST3 <118021999+K0ST3@users.noreply.github.com>
2023-01-19 22:40:51 +0100

    Add files via upload

---
+++ js/settingsSlick.js
@@ -0,0 +1,23 @@
+$('.team-wrap').slick({
+	arrows: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	mobileFirst: true,
+	
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
+})
+
